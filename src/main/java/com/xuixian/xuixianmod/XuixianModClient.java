41ea6e48d7caf5abe4cafba20fa654b47dfcc52e
受淦 周 <1534997318@qq.com>
2026-04-09 21:23:57 +0800

    第一版

---
+++ src/main/java/com/xuixian/xuixianmod/XuixianModClient.java
@@ -0,0 +1,11 @@
+package com.xuixian.xuixianmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class XuixianModClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
